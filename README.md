02cd8fd793e930ab5f952716461a2e19ec205cb1
klsierraj <klsierraj47@gmail.com>
2021-08-21 23:42:56 -0500

    styles and pagination

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ Initial Commit
 Fetch Characters from api - Done :white_check_mark:
 
 Tasks / TODO <br>
-Fetch Comics <br>
-Save in LocalStorage <br>
-Delete LocalStorage <br>
-Render Characters and Comics
-Pipe Filter
+Fetch Comics <br>  Done :white_check_mark:
+Save in LocalStorage <br>  Done :white_check_mark:
+Delete LocalStorage <br>  Done :white_check_mark:
+Render Characters and Comics 
+Pipe Filter  Done :white_check_mark:
 Pipe Pagination
 Add Styles And Responsive 
 Deploy to the server 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { CharactersComponent } from './components/characters/characters/characters.component';
 import { FilterPipeModule } from 'ngx-filter-pipe';
+import {NgxPaginationModule} from 'ngx-pagination'
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharactersComponent
+    CharactersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FilterPipeModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
