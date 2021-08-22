f3e3cebafdd84ac5b91d0642dd0579b1a4f4dad4
klsierraj <klsierraj47@gmail.com>
2021-08-22 00:12:03 -0500

    deploy

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export const Global = {
-    url: 'https://gateway.marvel.com:443/v1/public/characters?limit=30&apikey=f562216cbcc97b73d608a276f5ec57b4',
-    key: '?apikey=f562216cbcc97b73d608a276f5ec57b4',
-    comicsUrl: 'https://gateway.marvel.com:443/v1/public/comics?apikey=f562216cbcc97b73d608a276f5ec57b4'
+    url: 'https://gateway.marvel.com:443/v1/public/characters?limit=30&apikey=dce74c38aca11625adb7cfb4cf528cfa',
+    key: '?apikey=dce74c38aca11625adb7cfb4cf528cfa',
+    comicsUrl: 'https://gateway.marvel.com:443/v1/public/comics?apikey=dce74c38aca11625adb7cfb4cf528cfa'
 }
